591f79f2e94f9f226bfcf281a243efd8ca63eea3
? unknown ? <31632676+filip204@users.noreply.github.com>
2018-07-26 20:42:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 
 bot=commands.Bot(command_prefix="w.", description="STILL IN DEVELOPMENT")
 
-extensions=['General']
+extensions=['cogs.General', 'cogs.Moderation']
 
 for extension in extensions:
     bot.load_extension(extension)
